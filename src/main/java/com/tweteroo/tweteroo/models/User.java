934d8a371c967c88cd4355083001f854e9188f88
luiz <lguilhermefl@gmail.com>
2023-01-17 19:37:25 -0300

    feat: add constructor with user dto data

--- src/main/java/com/tweteroo/tweteroo/models/User.java
+++ src/main/java/com/tweteroo/tweteroo/models/User.java
@@ -1,11 +1,20 @@
 package com.tweteroo.tweteroo.models;
 
+import com.tweteroo.tweteroo.dtos.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class User {
+
+    public User(UserDTO data) {
+        this.username = data.username();
+        this.avatar = data.avatar();
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
