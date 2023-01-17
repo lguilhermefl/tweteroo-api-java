6dbbde956e05593846869dca987f5355e203eaab
luiz <lguilhermefl@gmail.com>
2023-01-17 19:34:26 -0300

    feat: create user

---
+++ src/main/java/com/tweteroo/tweteroo/controllers/SignUpController.java
@@ -0,0 +1,23 @@
+package com.tweteroo.tweteroo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.tweteroo.dtos.UserDTO;
+import com.tweteroo.tweteroo.repositories.UserRepository;
+
+@RestController
+@RequestMapping("/sign-up")
+public class SignUpController {
+    
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping
+    public void create(@RequestBody UserDTO req) {
+        repository.save(new User());
+    }
+}
