ad9f1f2611b790cabd29d85ad9f74d8cbc89a6cb
luiz <lguilhermefl@gmail.com>
2023-01-17 19:21:27 -0300

    feat: create user model

---
+++ src/main/java/com/tweteroo/tweteroo/model/User.java
@@ -0,0 +1,19 @@
+package com.tweteroo.tweteroo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column(length = 50, nullable = false)
+    String username;
+
+    @Column(length = 200, nullable = false)
+    String avatar;
+}
