26dcdc9d790aba615c0545d8362e7ef6496a5ee2
luiz <lguilhermefl@gmail.com>
2023-01-17 19:45:05 -0300

    feat: add get tweets

--- src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
@@ -1,6 +1,9 @@
 package com.tweteroo.tweteroo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,11 @@ public class TweetController {
     
     @Autowired
     private TweetRepository repository;
+
+    @GetMapping
+    public List<Tweet> listAll() {
+        return repository.findAll();
+    }
 
     @PostMapping
     public void create(@RequestBody TweetDTO req) {
