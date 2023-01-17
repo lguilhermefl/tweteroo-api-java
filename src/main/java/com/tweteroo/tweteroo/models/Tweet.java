2bbfaa32c4dd6ccf4d346d367385a71bbffacc27
luiz <lguilhermefl@gmail.com>
2023-01-17 19:43:42 -0300

    feat: add constructor with tweet dto data

--- src/main/java/com/tweteroo/tweteroo/models/Tweet.java
+++ src/main/java/com/tweteroo/tweteroo/models/Tweet.java
@@ -1,12 +1,21 @@
 package com.tweteroo.tweteroo.models;
 
+import com.tweteroo.tweteroo.dtos.TweetDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class Tweet {
     
+    public Tweet(TweetDTO data) {
+        this.username = data.username();
+        this.text = data.tweet();
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
