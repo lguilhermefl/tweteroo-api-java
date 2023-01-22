281c9bde8046fa560d0b7b45f5623bc7bcd1a46f
luiz <lguilhermefl@gmail.com>
2023-01-21 23:59:49 -0300

    fix: remove username

--- src/main/java/com/tweteroo/tweteroo/dtos/TweetDTO.java
+++ src/main/java/com/tweteroo/tweteroo/dtos/TweetDTO.java
@@ -3,12 +3,8 @@ package com.tweteroo.tweteroo.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record TweetDTO(
-
     @NotBlank
-    String username,
-    
-    @NotBlank
-    String tweet
+    String text
 ) {
     
 }
