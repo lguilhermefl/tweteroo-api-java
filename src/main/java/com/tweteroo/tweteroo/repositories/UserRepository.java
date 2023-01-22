af1b83aad60e2ab17bf5d2d6a528a5357301cada
luiz <lguilhermefl@gmail.com>
2023-01-21 23:41:27 -0300

    feat: create find user by username

--- src/main/java/com/tweteroo/tweteroo/repositories/UserRepository.java
+++ src/main/java/com/tweteroo/tweteroo/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.tweteroo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.tweteroo.models.AppUser;
 
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     
+    Optional<AppUser> findByUsername(String username);
 }
