900936d9c1b4b875ab009bb4cdf7aa26e7a5e523
luiz <lguilhermefl@gmail.com>
2023-01-22 02:35:44 -0300

    feat: add cross origin

--- src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.tweteroo.tweteroo.models.Tweet;
 import com.tweteroo.tweteroo.services.TweetService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/tweets")
 public class TweetController {
     
