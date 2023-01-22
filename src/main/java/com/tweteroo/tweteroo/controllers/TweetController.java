9c4b8b4b7392cda22a217732232f89b26a74d857
luiz <lguilhermefl@gmail.com>
2023-01-22 02:13:33 -0300

    feat: add get tweets with pagination

--- src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
@@ -1,17 +1,25 @@
 package com.tweteroo.tweteroo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.tweteroo.dtos.TweetDTO;
+import com.tweteroo.tweteroo.models.Tweet;
 import com.tweteroo.tweteroo.services.TweetService;
 
 @RestController
-@RequestMapping("/tweets")
+@RequestMapping("/api/tweets")
 public class TweetController {
     
     @Autowired
@@ -20,5 +28,12 @@ public class TweetController {
     @PostMapping
     public void create(@RequestHeader("User") String username, @RequestBody TweetDTO req) {
         service.save(username, req);
+    }
+
+    @GetMapping
+    public List<Tweet> getLastFiveTweets(@RequestParam String page) {
+        int pageNumber = Integer.parseInt(page);
+        Pageable sortedByIdDesc = PageRequest.of(pageNumber, 5, Sort.by("id").descending());
+        return service.getTweets(sortedByIdDesc);
     }
 }
