d94178237d84a7da53e5c3816b52a80619dfb6de
luiz <lguilhermefl@gmail.com>
2023-01-22 01:39:55 -0300

    feat: add find all sorted by id desc

--- src/main/java/com/tweteroo/tweteroo/repositories/TweetRepository.java
+++ src/main/java/com/tweteroo/tweteroo/repositories/TweetRepository.java
@@ -1,9 +1,11 @@
 package com.tweteroo.tweteroo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.tweteroo.models.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-    
+    Page<Tweet> findAllSortedByIdDesc(Pageable pageable);
 }
