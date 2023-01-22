ef39c63b1f2d6585ea2f333cfc6fd45b83944890
luiz <lguilhermefl@gmail.com>
2023-01-22 02:27:29 -0300

    feat: add get tweets by user

--- src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -35,5 +36,10 @@ public class TweetController {
         int pageNumber = Integer.parseInt(page);
         Pageable sortedByIdDesc = PageRequest.of(pageNumber, 5, Sort.by("id").descending());
         return service.getTweets(sortedByIdDesc);
+    }
+
+    @GetMapping("/{username}")
+    public List<Tweet> getTweetsByUser(@PathVariable String username) {
+        return service.getTweetsByUser(username);
     }
 }
