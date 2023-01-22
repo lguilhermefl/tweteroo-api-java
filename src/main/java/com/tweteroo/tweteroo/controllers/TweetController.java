5da6890d02b19c59307b732ea57f0490cb96de50
luiz <lguilhermefl@gmail.com>
2023-01-22 01:38:11 -0300

    feat: add create tweet

--- src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controllers/TweetController.java
@@ -1,32 +1,24 @@
 package com.tweteroo.tweteroo.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.tweteroo.dtos.TweetDTO;
-import com.tweteroo.tweteroo.models.Tweet;
-import com.tweteroo.tweteroo.repositories.TweetRepository;
+import com.tweteroo.tweteroo.services.TweetService;
 
 @RestController
 @RequestMapping("/tweets")
 public class TweetController {
     
     @Autowired
-    private TweetRepository repository;
-
-    @GetMapping
-    public List<Tweet> listAll() {
-        return repository.findAll();
-    }
+    private TweetService service;
 
     @PostMapping
-    public void create(@RequestBody TweetDTO req) {
-        repository.save(new Tweet(req));
+    public void create(@RequestHeader("User") String username, @RequestBody TweetDTO req) {
+        service.save(username, req);
     }
 }
